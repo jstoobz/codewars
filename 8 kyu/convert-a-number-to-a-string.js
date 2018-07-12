65be1a160ee3802ac1018ae2db3b11df4d5839ce
James Stephens <stephensja7@gmail.com>
2018-07-12 19:00:46 -0400

    added kata solution

---
+++ 8 kyu/convert-a-number-to-a-string.js
@@ -0,0 +1,8 @@
+// Kata Link: https://codewars.com/kata/convert-a-number-to-a-string
+
+function numberToString(num) {
+  return num.toString();
+}
+
+let result = numberToString(67);
+console.log(result);

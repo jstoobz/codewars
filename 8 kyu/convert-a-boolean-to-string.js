9d30f8670656f751068e6b310b8623e40804694c
James Stephens <stephensja7@gmail.com>
2018-07-12 18:55:32 -0400

    added kata solution

---
+++ 8 kyu/convert-a-boolean-to-string.js
@@ -0,0 +1,8 @@
+// Kata Link: https://codewars.com/kata/convert-a-boolean-to-a-string
+
+function booleanToString(b){
+  return (b) ? 'true' : 'false';
+}
+
+let result = booleanToString(true);
+console.log(result);

e6c6cbb6a661c91d08fe017dd4076fdc38614a0c
James Stephens <stephensja7@gmail.com>
2018-07-12 19:40:41 -0400

    added kata solution

---
+++ 8 kyu/grasshopper-function-syntax-debugging.js
@@ -0,0 +1,8 @@
+// Kata Link: https://www.codewars.com/kata/grasshopper-function-syntax-debugging
+
+function main(verb, noun) {
+  return `${verb}${noun}`;
+}
+
+let result = main('take ', 'item');
+console.log(result);

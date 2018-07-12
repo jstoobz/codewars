af279665286570afb7017f45385052f3b2b8747e
James Stephens <stephensja7@gmail.com>
2018-07-12 09:41:18 -0400

    added kata solution

---
+++ 8 kyu/training-js-1-create-your-frist-js-function-and-print-hello-world.js
@@ -0,0 +1,6 @@
+// Kata Link: https://www.codewars.com/kata/training-js-number-1-create-your-first-js-function-and-print-helloworld
+
+function helloWorld() {
+  var str = 'Hello World!';
+  console.log(str);
+}

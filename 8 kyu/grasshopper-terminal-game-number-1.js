bffa48297dc115098464f6a0633e1db3d124723e
James Stephens <stephensja7@gmail.com>
2018-07-12 19:34:24 -0400

    added kata solution

---
+++ 8 kyu/grasshopper-terminal-game-number-1.js
@@ -0,0 +1,9 @@
+// Kata Link: https://codewars.com/kata/grasshopper-terminal-game-number-1
+
+function Hero (name) {
+  this.name = name || 'Hero',
+  this.position = '00',
+  this.health = 100,
+  this.damage = 5,
+  this.experience = 0
+}

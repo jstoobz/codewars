1118c8ff64b5514e6a2ae5794ff948844e0e9305
James Stephens <stephensja7@gmail.com>
2018-07-12 19:06:52 -0400

    added kata solution

---
+++ 8 kyu/convert-a-string-to-an-array.js
@@ -0,0 +1,8 @@
+// Kata Link: https://codewars.com/kata/convert-a-string-to-an-array
+
+function stringToArray(string){
+  return string.split(' ');
+}
+
+let result = stringToArray('Robin Singh');
+console.log(result);

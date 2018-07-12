541b30ecfde22fcd2a8c0aa6aa5456cab2c2207a
James Stephens <stephensja7@gmail.com>
2018-07-12 15:20:10 -0400

    added kata solution

---
+++ 8 kyu/switch-it-up.js
@@ -0,0 +1,8 @@
+// Kata Link: https://codewars.com/kata/training-js-number-3-basic-data-types-string
+
+function switchItUp(number){
+  return ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'][number];
+}
+
+let result = switchItUp(5);
+console.log(result);

8e1bbbb5310f7729e481d22c5a68e7d48d5defb0
James Stephens <stephensja7@gmail.com>
2018-07-12 17:06:45 -0400

    added kata solution

---
+++ 8 kyu/beginner-series-4-cockroach.js
@@ -0,0 +1,8 @@
+// Kata Link: https://codewars.com/kata/beginner-series-number-4-cockroach
+
+function cockroachSpeed(s) {
+  return Math.floor(s * 27.778);
+}
+
+let result = cockroachSpeed(1.09);
+console.log(result);

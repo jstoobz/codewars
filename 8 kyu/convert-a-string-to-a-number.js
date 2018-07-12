23c164e2645d2f302f8e7f8be2d0ddb66ecf3b85
James Stephens <stephensja7@gmail.com>
2018-07-12 19:04:36 -0400

    added kata solution

---
+++ 8 kyu/convert-a-string-to-a-number.js
@@ -0,0 +1,8 @@
+// Kata Link: https://codewars.com/kata/convert-a-string-to-a-number
+
+var stringToNumber = function(str){
+  return Number(str);
+}
+
+let result = stringToNumber('1234');
+console.log(result);

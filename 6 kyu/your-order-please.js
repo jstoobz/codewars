f69d101c3bfed58afa1f855a5135bdcc45459fb1
James Stephens <stephensja7@gmail.com>
2018-08-01 13:22:46 -0400

    updated kata solution

--- 6 kyu/your-order-please.js
+++ 6 kyu/your-order-please.js
@@ -1,8 +1,7 @@
 // Kata Link: https://codewars.com/kata/your-order-please
 
 const order = (words) => {
-  if (!words.length) return '';
-  return words.split(' ').map(el => el.match(/\d+/)).sort((a, b) => a[0] > b[0]).map(el => el.input).join(' ');
+  return words.split(' ').sort((a, b) => a.match(/\d/) - b.match(/\d/)).join(' ');
 };
 
 console.log(order('is2 Thi1s T4est 3a'));
